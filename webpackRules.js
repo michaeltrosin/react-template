6ab55c0a5a9a3b75ba5cfa36fc969f8a30ad8aee
Ra6nar0k21 | Michael <38357618+michaeltrosin@users.noreply.github.com>
2021-12-23 08:22:17 +0100

    Create webpackRules.js

---
+++ webpackRules.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const rules = require('./webpackRules');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/renderer.tsx',
+  target: 'electron-renderer',
+  devtool: 'source-map',
+  devServer: {
+    static: path.join(__dirname, 'bin'), // /renderer.js'),
+    compress: true,
+
+    port: 9000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@asset': path.resolve(__dirname, 'asset'),
+    },
+    extensions: ['', '.jsx', '.tsx', '.ts', '.js', '.png', '.jpg', '.svg'],
+    fallback: rules.fallback,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        //include: /src/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      rules.source,
+      rules.inline,
+      rules.resource,
+      rules.svg,
+    ],
+  },
+  output: {
+    path: __dirname + '/bin',
+    filename: 'renderer.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  'externals': {
+    'electron': 'require(\'electron\')',
+    'child_process': 'require(\'child_process\')',
+    'fs': 'require(\'fs\')',
+    'path': 'require(\'path\')',
+  },
+};
