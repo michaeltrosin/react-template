0946ede606cd0bd123565146c2186b2f1a564b17
michael <michael@ra6.io>
2022-01-30 15:19:43 +0100

    y

--- webpackRules.js
+++ webpackRules.js
@@ -1,62 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
-const rules = require('./webpackRules');
-
 module.exports = {
-  mode: 'development',
-  entry: './src/renderer.tsx',
-  target: 'electron-renderer',
-  devtool: 'source-map',
-  devServer: {
-    static: path.join(__dirname, 'bin'), // /renderer.js'),
-    compress: true,
-
-    port: 9000,
+  resource: {
+    test: /\.png/,
+    type: 'asset/resource',
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@asset': path.resolve(__dirname, 'asset'),
-    },
-    extensions: ['', '.jsx', '.tsx', '.ts', '.js', '.png', '.jpg', '.svg'],
-    fallback: rules.fallback,
+  inline: {
+    // test: /\.svg/,
+    // type: 'asset/inline',
   },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        //include: /src/,
-        exclude: /node_modules/,
-        use: [{ loader: 'ts-loader' }],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      rules.source,
-      rules.inline,
-      rules.resource,
-      rules.svg,
-    ],
+  source: {
+    test: /\.txt|\.traineddata/,
+    type: 'asset/source',
   },
-  output: {
-    path: __dirname + '/bin',
-    filename: 'renderer.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  'externals': {
-    'electron': 'require(\'electron\')',
-    'child_process': 'require(\'child_process\')',
-    'fs': 'require(\'fs\')',
-    'path': 'require(\'path\')',
-  },
+  svg: {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  }, 
+  fallback: {
+    'stream': false,
+    'fs': false,
+    'path': false,
+  }
 };
